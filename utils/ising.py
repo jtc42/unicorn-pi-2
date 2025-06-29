e68425dfd3c29c5ea8ba3921b2e568487d52a096
Joel T. Collins <jtc42@users.noreply.github.com>
2025-06-29 13:18:44 +0100

    feat: add temporal smoothing to ising animation

--- utils/ising.py
+++ utils/ising.py
@@ -11,6 +11,7 @@ class IsingModel:
         initial_temperature=2.0,
         annealing_rate=0.05,
         min_temperature=0.5,
+        temporal_window=4,  # Number of grids to average for smoothing
     ):
         self.width = width
         self.height = height
@@ -20,8 +21,16 @@ class IsingModel:
         self.temperature = initial_temperature
         self.annealing_rate = annealing_rate
         self.min_temperature = min_temperature
+        self.temporal_window = temporal_window
         self._grid = np.random.choice([-1, 1], size=(self.height, self.width))
         self._last_update = -1
+        self._history = np.empty(
+            (self.temporal_window, self.height, self.width), dtype=np.float32
+        )
+        self._history[0] = self._grid
+        self._history_ptr = 1  # Next index to write
+        self._history_filled = 1  # Number of valid grids
+        self._history_sum = self._grid.astype(np.float32).copy()  # Running sum for mean
 
     def _heatbath_step(self):
         """
@@ -64,10 +73,19 @@ class IsingModel:
                 self.min_temperature,
                 self.initial_temperature - t * self.annealing_rate,
             )
-
-        # Get the spin value at the requested position
+            # Remove the old grid from the sum if buffer is full
+            if self._history_filled == self.temporal_window:
+                self._history_sum -= self._history[self._history_ptr]
+            else:
+                self._history_filled += 1
+            # Add the new grid to the sum and buffer
+            self._history[self._history_ptr] = self._grid
+            self._history_sum += self._grid
+            self._history_ptr = (self._history_ptr + 1) % self.temporal_window
+        # Compute the temporally averaged grid (O(1))
+        avg_grid = self._history_sum / self._history_filled
         xi, yi = int(x) % self.width, int(y) % self.height
-        spin = self._grid[yi, xi]
+        spin = avg_grid[yi, xi]
 
         self._last_update = t  # Update last update time
 
@@ -91,6 +109,14 @@ class IsingModel:
 
         if min_temp is not None:
             self.min_temperature = min_temp
+
+        self._history = np.empty(
+            (self.temporal_window, self.height, self.width), dtype=np.float32
+        )
+        self._history[0] = self._grid
+        self._history_ptr = 1
+        self._history_filled = 1
+        self._history_sum = self._grid.astype(np.float32).copy()
 
 
 # Singleton instance for module-level functions
